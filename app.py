06cdbeeab2c40c313241a38798b4d984617accac
Joe Hawk <yarwalker@gmail.com>
2023-01-27 12:51:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,5 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        logger.error("Bot stopped!")
-
-        
+        logger.error("Bot stopped!")   
      
